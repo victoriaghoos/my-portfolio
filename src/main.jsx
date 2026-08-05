861ba009bf14ae5b293536c248f4d8e9c9f33b52
victoriaghoos <ghoosvictoria@gmail.com>
2026-08-05 11:58:12 +0200

    Install fontsource poppins

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { I18nextProvider } from 'react-i18next';
 import './index.css';
 import App from './App';
 import i18n from './i18n';
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/700.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
